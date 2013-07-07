9d1975e1b9cf7077458f16b8c583c7315da0e178
AnhDN <anhdn@sslash.com>
2013-07-07 23:22:56 +0700

    'index.html'

--- web.js
+++ web.js
@@ -2,16 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-
 app.get('/', function(request, response) {
-var fs = require('fs');
-	//var buffer = new buffer ();
-	response.send('hello');
-
+  response.send('Hello World!');
 });
-/*
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-*/
