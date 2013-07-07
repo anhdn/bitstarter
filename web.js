b43e6b31508966a6eaa677309df08d44e15a1e57
AnhDN <anhdn@sslash.com>
2013-07-07 23:58:03 +0700

    'index.html'

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer =  new Buffer(256);;
+var buffer =  new Buffer();;
 response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
   //response.send('Hello World!');
 });
