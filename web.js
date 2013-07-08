a26e6430e807f5fc7edb53e3f851d38522289311
AnhDN <anhdn@sslash.com>
2013-07-08 10:39:49 +0700

    'test'

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-	fs.readFile('index.html', function (err, data) {
+	fs.readFileSync('index.html', function (err, data) {
 	  if (err) throw err;
 	  var buffer = new Buffer(data, "utf-8");
 	  response.send(buffer.toString('utf-8'));
